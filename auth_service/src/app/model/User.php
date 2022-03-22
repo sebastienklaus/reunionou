8bf7afa7166f386de14de89243a0a042238d0fde
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-22 11:50:42 +0100

    add User model

---
+++ auth_service/src/app/model/User.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace reunionou\auth\app\model;
+
+
+use \Psr\Http\Message\ServerRequestInterface as Request ;
+use \Psr\Http\Message\ResponseInterface as Response ;
+use Psr\Container\ContainerInterface;
+
+class User extends \Illuminate\Database\Eloquent\Model {
+
+    protected $table      = 'users';  /* le nom de la table */
+    protected $primaryKey = 'id';
+    public    $timestamps = true;    /* si vrai la table doit contenir
+                                        les deux colonnes updated_at,
+                                        created_at */
+
+}
