b2749dcd7301bc412d4484e16c0b7323831c76d8
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-24 10:51:57 +0100

    remove unuse controller

--- backoffice_service/public/index.php
+++ backoffice_service/public/index.php
@@ -2,7 +2,6 @@
 
 require_once  __DIR__ . '/../src/vendor/autoload.php';
 
-use reunionou\backoffice\app\controller\BackOfficeAuthController as BackOfficeAuthController;
 
 //Loading configs
 $settings = require_once __DIR__. '/../src/app/conf/settings.php';
@@ -14,8 +13,6 @@ $app = new \Slim\App(new \Slim\Container($app_config));
 require_once __DIR__ . '/../src/app/routes/routesAuth.php';
 
 require_once __DIR__ . '/../src/app/routes/routesEvents.php';
-
-
 
 
 $app->run();
