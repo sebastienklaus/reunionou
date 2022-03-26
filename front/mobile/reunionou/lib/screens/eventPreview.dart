83b8eb8921d2a1c8a1ccbf286279951c46b85f93
Malek BK <malekbk98@gmail.com>
2022-03-26 21:04:04 +0100

    check if auth user is event owner

--- front/mobile/reunionou/lib/screens/eventPreview.dart
+++ front/mobile/reunionou/lib/screens/eventPreview.dart
@@ -39,62 +39,69 @@ class _HomeScreenState extends State<EventPreviewScreen> {
           return const Center(
             child: CircularProgressIndicator(),
           );
-        } else {}
-        return DefaultTabController(
-          length: 1 == 1 ? 5 : 4,
-          child: Scaffold(
-            endDrawer: NavigationDrawerWidget(),
-            appBar: AppBar(
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back, color: Colors.white),
-                onPressed: () => Navigator.of(context).pop(),
+        } else {
+          return DefaultTabController(
+            length:
+                widget.event.user_id == context.read<DataLoader>().getUser().id
+                    ? 5
+                    : 4,
+            child: Scaffold(
+              endDrawer: NavigationDrawerWidget(),
+              appBar: AppBar(
+                leading: IconButton(
+                  icon: const Icon(Icons.arrow_back, color: Colors.white),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+                bottom: TabBar(
+                  tabs: [
+                    const Tab(icon: Icon(Icons.event)),
+                    const Tab(icon: Icon(Icons.badge)),
+                    const Tab(icon: Icon(Icons.people)),
+                    const Tab(icon: Icon(Icons.comment)),
+                    if (widget.event.user_id ==
+                        context.read<DataLoader>().getUser().id)
+                      const Tab(icon: Icon(Icons.settings))
+                  ],
+                ),
+                title: Text(widget.event.title.length > 15
+                    ? widget.event.title.substring(0, 15) + "..."
+                    : widget.event.title),
+                centerTitle: true,
               ),
-              bottom: const TabBar(
-                tabs: [
-                  Tab(icon: Icon(Icons.event)),
-                  Tab(icon: Icon(Icons.badge)),
-                  Tab(icon: Icon(Icons.people)),
-                  Tab(icon: Icon(Icons.comment)),
-                  if (1 == 1) Tab(icon: Icon(Icons.settings))
+              body: TabBarView(
+                children: [
+                  EventDetails(
+                    event: widget.event,
+                  ),
+                  OrganizerDetails(
+                    event: widget.event,
+                  ),
+                  const ParticipantsList(),
+                  const CommentsList(),
+                  if (widget.event.user_id ==
+                      context.read<DataLoader>().getUser().id)
+                    Column(
+                      children: [
+                        const SpacerWidget(space: 30),
+                        const Text(
+                          "Modifier l'événement",
+                          style: TextStyle(
+                              fontSize: 25.0,
+                              color: Colors.blueGrey,
+                              letterSpacing: 2.0,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        Expanded(
+                          child: ManageEventScreen(
+                              event: widget.event, action: 'update'),
+                        ),
+                      ],
+                    )
                 ],
               ),
-              title: Text(widget.event.title.length > 15
-                  ? widget.event.title.substring(0, 15) + "..."
-                  : widget.event.title),
-              centerTitle: true,
             ),
-            body: TabBarView(
-              children: [
-                EventDetails(
-                  event: widget.event,
-                ),
-                OrganizerDetails(
-                  event: widget.event,
-                ),
-                const ParticipantsList(),
-                const CommentsList(),
-                if (1 == 1)
-                  Column(
-                    children: [
-                      const SpacerWidget(space: 30),
-                      const Text(
-                        "Modifier l'événement",
-                        style: TextStyle(
-                            fontSize: 25.0,
-                            color: Colors.blueGrey,
-                            letterSpacing: 2.0,
-                            fontWeight: FontWeight.w400),
-                      ),
-                      Expanded(
-                        child: ManageEventScreen(
-                            event: widget.event, action: 'update'),
-                      ),
-                    ],
-                  )
-              ],
-            ),
-          ),
-        );
+          );
+        }
       },
     );
   }
