240ae7352e5f33ed28fcbdf8c682c807c65ff822
sebastienklaus <sebastienklaus7@gmail.com>
2022-03-28 13:48:00 +0200

    add middlewaire checkauth

--- backoffice_service/src/app/routes/routesAuth.php
+++ backoffice_service/src/app/routes/routesAuth.php
@@ -13,12 +13,15 @@ $app->post('/users[/]', BackOfficeAuthController::class . ':createUser')
     ->setName('createUser');
 
 $app->get('/users[/]', BackOfficeAuthController::class . ':getUsers')
+    ->add(Middleware::class . ':checkAuth')
     ->setName('getUsers');
      
 $app->put('/users/{id}[/]', BackOfficeAuthController::class . ':updateUser')
+    ->add(Middleware::class . ':checkAuth')
     ->setName('createUser');
     
 $app->get('/users/{id}[/]', BackOfficeAuthController::class . ':getUserById')
+    ->add(Middleware::class . ':checkAuth')    
     ->setName('getUserById');
 
 $app->post('/authAdmin[/]', BackOfficeAuthController::class . ':authenticateAdmin')
