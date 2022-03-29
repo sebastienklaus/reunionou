9fb987842814af98a6e5b722d5a495e6359de7c4
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2022-03-29 15:12:44 +0200

    create member message error cleaned

--- backoffice_service/src/app/controller/BackOfficeMembersController.php
+++ backoffice_service/src/app/controller/BackOfficeMembersController.php
@@ -69,7 +69,7 @@ class BackOfficeMembersController
         //         $pathForUser = null;
         //     };
 
-        if (!isset($received_member['user_id'])) {
+        if (!isset($received_member['user_id']) ) {
             $received_member['user_id'] = null;
         }
 
